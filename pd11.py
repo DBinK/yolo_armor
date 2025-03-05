79df5f0d748d5acca9d00073184b608ce3d913cc
DBinK <DBinKv1@Gmail.com>
2025-03-05 11:28:29 +0800

    refactor(320): 优化模型推理性能测试方法
    - 修改推理时间计算方式，跳过前 100 帧
    - 更新结果统计，包括总有效推理时间和每帧平均推理时间
    - 调整模型加载路径和测试参数
    - 优化代码结构，移除不必要的注释和冗余逻辑

--- pd11.py
+++ pd11.py
@@ -15,9 +15,9 @@ from ultralytics import YOLO
 
 # model = YOLO("./320/320.pt")  
 # model.to(device='cpu')
-model = YOLO("D:\\IT\\yolo_armor\\320\\320_openvino_model")  
+# model = YOLO("D:\\IT\\yolo_armor\\320\\320_openvino_model")  
 # model = YOLO("D:\\IT\\yolo_armor\\320\\320_half_openvino_model")  
-# model = YOLO("D:\\IT\\yolo_armor\\320\\320_int8_openvino_model")  
+model = YOLO("D:\\IT\\yolo_armor\\320\\320_int8_openvino_model")  
 
 # model = YOLO("D:\\IT\\yolo_armor\\runs\\detect\\train12\\weights\\best.pt")  
 
@@ -39,7 +39,7 @@ tg_count = 0
 class_counts = {}
 
 # 设置忽略的帧数, 等待推理帧率稳定
-skip_frames = 50  
+skip_frames = 100
 
 # 读取视频帧并进行推理
 while cap.isOpened():
@@ -54,15 +54,10 @@ while cap.isOpened():
         print(f"跳过前 {frame_count} 帧...")
 
     # # 跳过前 N 帧
-    # if frame_count > skip_frames:
-    #     start_time = time.time()  # 开始计时
-
-    # # 跳过前 N 帧
-    if frame_count == 50:
+    if frame_count == skip_frames:
         start_time = time.time()  # 开始计时
     
     # 推理
-    # results = model(frame)
     results = model.predict(frame, save=False, imgsz=320)
     
     # # 获取检测结果
@@ -89,12 +84,6 @@ while cap.isOpened():
     # if cv2.waitKey(1) & 0xFF == ord("q"):
     #     break            
 
-    # 跳过前 N 帧
-    # if frame_count > skip_frames:
-    #     end_time = time.time()  # 结束计时
-    #     inference_time = end_time - start_time  # 记录推理时间
-    #     inference_times.append(inference_time)
-    #     print(f"帧 {frame_count}: 推理时间 = {inference_time*1000:.4f} 秒")
 
 end_time = time.time()  # 结束计时
 spend_time = end_time - start_time  # 记录总推理时间
@@ -105,19 +94,13 @@ fps = 1 / average_inference_time
 # 释放视频捕获
 cap.release()
 
-# 计算性能指标
-# average_inference_time = sum(inference_times) / len(inference_times)
-# max_inference_time = max(inference_times)
-# min_inference_time = min(inference_times)
-# fps = frame_count / sum(inference_times)
 
 # 打印结果
-print(f"处理帧数: {frame_count}")
-print(f"平均推理时间: {average_inference_time:.4f} 秒")
-# print(f"瞬时最大推理时间: {max_inference_time:.4f} 秒")
-# print(f"瞬时最小推理时间: {min_inference_time:.4f} 秒")
+print(f"处理帧数: {frame_count} - {skip_frames} = {frame_count-skip_frames}")
+print(f"总有效推理时间: {spend_time:.4f} 秒")
+print(f"平均推理时间: {average_inference_time*1000:.2f} ms")
 print(f"平均帧率: {fps:.2f} FPS")
-print(f"识别到 B3 的次数: {tg_count}")
+print(f"识别到 目标 的次数: {tg_count}")
 
 # 打印每个类别的计数
 print("每个类别的计数:")
